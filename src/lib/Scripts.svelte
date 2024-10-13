c678d7e92fbd0214ddeb3a1b9ffe7a9bf46402fc
stordahl <jacob@stordahl.dev>
2024-10-13 18:40:42 +0000

    add counterscale

---
+++ src/lib/Scripts.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { browser } from "$app/environment";
+	import { onMount } from "svelte";
+
+  onMount(() => {
+    // @ts-expect-error counter scale on window
+    if(import.meta.env.PROD && window.counterscale) {
+      // @ts-expect-error counter scale on window
+      window.counterscale = {
+        q: [["set", "siteId", "dotdev-analytics"], ["trackPageview"]],
+      };
+    }
+  })
+</script>
+
+{#if browser && import.meta.env.PROD}
+  <script
+    id="counterscale-script"
+    src="https://counterscale.stordahldev.workers.dev/tracker.js"
+    defer
+  ></script>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import './style.css';
 	import Header from './Header.svelte';
 	import Footer from './Footer.svelte';
+	import Scripts from '$lib/Scripts.svelte';
 
 	let { children } = $props();
 </script>
@@ -9,3 +10,5 @@
 <Header />
 {@render children()}
 <Footer />
+
+<Scripts />

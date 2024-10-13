8ff0b48a482f566d976d42b9ff41cbe03bc1a702
stordahl <jacob@stordahl.dev>
2024-10-13 18:54:24 +0000

    delay load of tracker

--- src/lib/Scripts.svelte
+++ src/lib/Scripts.svelte
@@ -2,6 +2,8 @@
   import { browser } from "$app/environment";
 	import { onMount } from "svelte";
 
+  let loadTracker = $state(false);
+
   onMount(() => {
     // @ts-expect-error counter scale on window
     if(import.meta.env.PROD && window.counterscale) {
@@ -9,11 +11,13 @@
       window.counterscale = {
         q: [["set", "siteId", "dotdev-analytics"], ["trackPageview"]],
       };
+
+      loadTracker = true;
     }
   })
 </script>
 
-{#if browser && import.meta.env.PROD}
+{#if browser && import.meta.env.PROD && loadTracker}
   <script
     id="counterscale-script"
     src="https://counterscale.stordahldev.workers.dev/tracker.js"

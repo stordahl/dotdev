bf6376a66c2b447427b96e72e42ee69b6e3efb0b
stordahl <jacob@stordahl.dev>
2024-10-13 21:00:14 +0000

    fix: update stylitics content

--- src/routes/work/stylitics/+page.svelte
+++ src/routes/work/stylitics/+page.svelte
@@ -5,22 +5,22 @@
 	const content = [
 		{
 			content:
-				'In Q3 of 2023, while at Stylitics, I was tasked with designing a system that would allow my team to monitor run-time errors from our many Third-Party JavaScript applications. Our suite of products are all client-side rendered widgets that require some configuration to render properly. This paradigm limits our implicit visibility into the run time of our applications, across the many websites they are running within.',
+				'In Q2 of 2024, I was thinking about two specific problems my team was trying to solve - client specific style themes for our white label products, and a/b testing component styles. The real problem for us was that loading more than one stylesheet on every client site would balloon our CDN egress, and thus cost. The solution I designed to solve this problem made use of a Cloudflare worker that could dynamically generate CSS files made up of existing pre-built files. Our products can construct a request to this worker, and based on the parameters of that request, receive a dyamically generated stylesheet that includes all the disperate styles needed for the particular experience. We deployed this system in Q3 2024, and have rolled it out to a few clients.',
 			title: 'Dynamic CSS at the Edge'
 		},
 		{
 			content:
-				'In Q3 of 2023, while at Stylitics, I was tasked with designing a system that would allow my team to monitor run-time errors from our many Third-Party JavaScript applications. Our suite of products are all client-side rendered widgets that require some configuration to render properly. This paradigm limits our implicit visibility into the run time of our applications, across the many websites they are running within.',
+				'In Q3 of 2023, I was tasked with designing a system that would allow my team to monitor run-time errors from our many Third-Party JavaScript applications. Our suite of products are all client-side rendered widgets that require some configuration to render properly. This paradigm limits our implicit visibility into the run time of our applications, across the many websites they are running within. To solve this, I built a simple cloud function for ingesting events, and a client SDK for integration with our various products.',
 			title: 'Error Monitoring Service',
 			link: '/writing/third-party-errors',
 			linkText: 'read more'
 		},
 		{
 			content:
-				'In Q3 of 2023, while at Stylitics, I was tasked with designing a system that would allow my team to monitor run-time errors from our many Third-Party JavaScript applications. Our suite of products are all client-side rendered widgets that require some configuration to render properly. This paradigm limits our implicit visibility into the run time of our applications, across the many websites they are running within.',
+				'In Q4 2023 I began designing our shared component library that would power all of our products. The goal was not to build a true design system, but a library that could provide common UI functionality while still being easily stylable by consumers. I chose SvelteKit to build the library as it would allow us to build a Storybook-like sandbox to develop components in, and has a handy CLI for building libraries. The library has expanded greatly and is now being used (to some extent) in all of our products.',
 			title: 'Svelte Component Library',
-			link: '/writing/third-party-errors',
-			linkText: 'read more'
+			link: 'https://web-assets.stylitics.com/ui/latest/index.html',
+			linkText: 'check it out!'
 		}
 	];
 </script>

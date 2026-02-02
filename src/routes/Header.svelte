a669501ee2b1e9e5b5cb7a876996dc40c7fdb168
stordahl <jacob@stordahl.dev>
2026-02-01 18:27:06 -0600

    feat: add online status via discord/lanyard

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import DiscordStatus from '$lib/DiscordStatus.svelte';
+</script>
+
 <header>
 	<a href="/" aria-label="home page">
 		<img src="/images/star-pair.svg" alt="a pair of star icons" />
@@ -6,7 +10,8 @@
 	<nav>
 		<a href="/work">work</a>
 		<a href="/writing">writing</a>
-    <a href="/sketch-book">sketches</a>
+		<a href="/sketch-book">sketches</a>
+		<DiscordStatus />
 	</nav>
 </header>
 
@@ -21,6 +26,7 @@
 
 	nav {
 		display: flex;
+		align-items: center;
 		gap: 10px;
 	}
 
@@ -29,16 +35,15 @@
 		font-size: 1.25rem;
 	}
 
-  a:has(img) {
-        transform: scale(0.8);
-    transition: transform 0.2s ease-in-out 0s;
-    border: none;
-    &:hover {
-      transform: scale(0.9);
-    }
-    &:after {
-      display: none;
-    }
-
-  }
+	a:has(img) {
+		transform: scale(0.8);
+		transition: transform 0.2s ease-in-out 0s;
+		border: none;
+		&:hover {
+			transform: scale(0.9);
+		}
+		&:after {
+			display: none;
+		}
+	}
 </style>
